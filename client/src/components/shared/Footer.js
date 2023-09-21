1c605ed2b1e446232e0e03f489804d74b78f3a75
uprising_rk <44646459+upringrk@users.noreply.github.com>
2023-09-21 21:22:17 +0530

    changes to footer

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -3,14 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-     <footer className="text-center text-white " style={{backgroundColor: '#21081a'}}>
+     <footer className="text-center text-white fixed-bottom " style={{backgroundColor: '#22081a'}}>
   {/* Grid container */}
-  <div className="container p-4" />
+  {/* <div className="container p-5" /> */}
   {/* Grid container */}
   {/* Copyright */}
   <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
-    © 2020 Copyright:
-    <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    © 2023 Copyright:
+    <div onClick={(e) => {window.location.href ='mailto:example@email.com';}}>any thing here </div>
+    <a className="text-white" href="mailto:kumarraushan40800@gmail.com"> My EMail</a>
   </div>
   {/* Copyright */}
 </footer>
